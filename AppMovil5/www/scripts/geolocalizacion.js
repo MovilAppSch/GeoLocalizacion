e0450341de47b5eda1ffe70eb395e97a111283e7
Damián <sch.damian@hotmail.com>
2018-02-11 11:35:19 -0300

    geolocalizacion.js

---
+++ AppMovil5/www/scripts/geolocalizacion.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    $('#btnMostrarMapa').click(getDatos);
+
+    function getDatos() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError
+            , {
+                maximumAge: 300000,
+                timeout: 15000,
+                enableHighAccuracy: true
+            });
+    }
+        function onSuccess(position) {
+
+            var cusLat = position.coords.latitude;
+            var cusLon = position.coords.longitude;
+            document.getElementById("txtLat").value = cusLat;
+            document.getElementById("txtLon").value = cusLon;
+            var coords = new google.maps.LatLng(cusLat, cusLon);
+            var opciones = {
+                center: coords, zoom: 15
+            };
+            var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+            var marcador = new google.maps.Marker({
+                position: coords,
+                map: mapa,
+                title: "Mi ubicacion",
+                animation: google.maps.Animation.DROP
+            })
+        }
+
+        function onError(err) {
+            alert.log("codigo de err:" + err.code + "msj = " + err.message);
+        }
+})
+
+
--- AppMovil5/www/scripts/index.js
+++ AppMovil5/www/scripts/index.js
@@ -11,7 +11,7 @@
         // Controlar la pausa de Cordova y reanudar eventos
         document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
-        $('#btnMostrarMapa').click(getDatos);
+     //   $('#btnMostrarMapa').click(getDatos);
         // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
     };
 
@@ -23,45 +23,35 @@
         // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
     };
 
-    var onSuccess = function (position) {
-        alert("llego a onSucces");
+    function onSuccess(position) {
+
         var cusLat = position.coords.latitude;
         var cusLon = position.coords.longitude;
-
         document.getElementById("txtLat").value = cusLat;
         document.getElementById("txtLon").value = cusLon;
-
-        try {
-            var coords = new google.maps.LatLng(cusLat, cusLon);
-
-            var opciones = {
-                center: coords, zoom: 15
-            };
-
-            var mapa = new google.maps.Map(document.getElementById("map"), opciones);
-            var marcador = new google.maps.Marker({
-                position: coords,
-                map: mapa,
-                title: "Mi ubicacion",
-                animation: google.maps.Animation.DROP
-            });
-        }
-        catch (err) {
-            console.log(err.message);
-            alert(err.message);
-        }
+        var coords = new google.maps.LatLng(cusLat, cusLon);
+        var opciones = {
+            center: coords, zoom: 15
+        };
+        var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+        var marcador = new google.maps.Marker({
+            position: coords,
+            map: mapa,
+            title: "Mi ubicacion",
+            animation: google.maps.Animation.DROP
+        })
     }
 
-    var onError = function (err) {
+    function onError(err) {
         alert.log("codigo de err:" + err.code + "msj = " + err.message);
     }
 
     function getDatos() {
-        navigator.geolocation.watchPosition(onSuccess,onError
-        , {
-            maximumAge: 300000,
-            timeout: 10000,
-            enableHighAccuracy: true
-        });
+        navigator.geolocation.watchPosition(onSuccess, onError
+            , {
+                maximumAge: 300000,
+                timeout: 10000,
+                enableHighAccuracy: true
+            });
     }
 })();
